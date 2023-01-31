158f282694dfe4c11a9c7eee940a98ea462dabb0
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-01-31 19:44:52 +0200

    Add update_dns_record

--- main.py
+++ main.py
@@ -32,11 +32,42 @@ def filter_records(dns_records: dict,
     return filtered
 
 
+def update_dns_record(zone_id: str,
+                      api_token: str,
+                      record: dict) -> dict:
+    url = f"{BASE_URL}zones/{zone_id}/dns_records/{record['id']}"
+    print(url)
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.put(url,
+                            headers=headers,
+                            json=record,
+                            )
+
+    return response.json()
+
+
 def main():
     records = list_dns_records(zone_id=ZONE_ID, api_token=API_TOKEN)
-    print(json.dumps(records, indent=2))
+    # print(json.dumps(records, indent=2))
     filtered_records = filter_records(dns_records=records, domains=DOMAINS_TO_UPDATE, record_type="A")
+    # print(json.dumps(filtered_records, indent=2))
+
+    for r in filtered_records:
+        r["content"] = "1.1.1.1"
+        r["comment"] = "updated automatically by konso's updater script"
+        del r["created_on"]
+        del r["modified_on"]
+
     print(json.dumps(filtered_records, indent=2))
+
+    for r in filtered_records:
+        update_dns_record(zone_id=ZONE_ID,
+                          api_token=API_TOKEN,
+                          record=r)
+
 
 
 
